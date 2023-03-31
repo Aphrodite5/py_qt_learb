2714fa8fc7f66d069526a39aad60a684c5472031
Aphrodite5 <apollo.amazonios@gmail.com>
2023-03-31 10:09:16 +0530

    first_one

--- Py_qtie/btnlable.py
+++ Py_qtie/btnlable.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QFont
 import sys
 
 class Window(QWidget):
@@ -7,10 +7,19 @@ class Window(QWidget):
         super().__init__()
         self.setWindowTitle("Buttons")
         self.setWindowIcon(QIcon("qt.png"))
-        self.setGeometry(1300, 800, 1300, 800)
-
+        self.setGeometry(500, 500, 500, 500)
+        self.create_stuff()
     def create_stuff(self):
         btn = QPushButton("start", self)
+        btn.setGeometry(100, 100, 100, 100)
+        btn.setStyleSheet("background-color: red")
+        btn.setIcon(QIcon("football.png"))
+        btn.setFont(QFont("Time New Roman", 15))
+
+        lable = QLabel("button", self)
+        lable.move(100, 200)
+        lable.setStyleSheet("color.green")
+        lable.setFont(QFont("console",15))
 
 app = QApplication(sys.argv)
 window = Window()
